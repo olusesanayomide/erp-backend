6f99e48e04ab5917f5323ef15243da338b104fc3
Ayomide Olusesan <kidd37214@gmail.com>
2026-01-06 21:12:56 +0100

    feat: implement inventory management with stock movement DTO, controller, and service methods

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
-import { InventoryModule } from 'inventory/inventory.module';
+import { InventoryModule } from 'inventory module/inventory.module';
 
 @Module({
   imports: [InventoryModule],
