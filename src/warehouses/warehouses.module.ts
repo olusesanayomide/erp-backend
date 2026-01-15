74f65aeaab22a717e13bb09d7b0f29798ffe1bf5
Ayomide Olusesan <kidd37214@gmail.com>
2026-01-15 16:32:07 +0100

    update: updated waerhouse module

--- src/warehouses/warehouses.module.ts
+++ src/warehouses/warehouses.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WarehousesService } from './warehouses.service';
 import { WarehousesController } from './warehouses.controller';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Module({
   controllers: [WarehousesController],
-  providers: [WarehousesService],
+  providers: [WarehousesService, PrismaService],
 })
 export class WarehousesModule {}
