a1e0b0119c449a292593da136131dfeecb7d1dc2
Ayomide Olusesan <kidd37214@gmail.com>
2026-01-17 15:26:42 +0100

    feat(auth): implement secure login and jwt generation
    - Implement 'login' logic with bcrypt password comparison
    - Integrate @nestjs/jwt for access token generation
    - Add JWT payload containing user ID, email, and roles
    - Refactor secrets to use .env and @nestjs/config for security
    - Sanitize user response by removing sensitive password data

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot(),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   providers: [AuthService],
-  controllers: [AuthController]
+  controllers: [AuthController],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from './dto/signup.dto';
+import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+  ) {}
+
+  //   Register User
+  async register(dto: CreateUserDto) {
+    const saltOrRounds = 10;
+    const hashedPassword = await bcrypt.hash(dto.password, saltOrRounds);
+
+    const user = await this.prisma.user.create({
+      data: {
+        name: dto.name,
+        email: dto.email,
+        password: hashedPassword,
+        roles: {
+          connect: { name: dto.role },
+        },
+      },
+      include: { roles: true },
+    });
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password, ...result } = user;
+    return result;
+  }
+
+  // User Login
+  async login(dto: LoginDto) {
+    const user = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+      include: { roles: true },
+    });
+    if (!user) {
+      throw new UnauthorizedException('Credentials Incorrect');
+    }
+
+    const pwMatches = await bcrypt.compare(dto.password, user.password);
+    if (!pwMatches) {
+      throw new UnauthorizedException('Credentials Incorrect ');
+    }
+    // Generate Jwt Payload
+    const payload = {
+      sub: user.id,
+      email: user.email,
+      roles: user.roles.map((r) => r.name),
+    };
+    // sign the  token and retur it
+    const token = await this.jwt.signAsync(payload);
+    return {
+      message: 'Logged in Successfully',
+      access_token: token,
+      user: {
+        email: user.email,
+        name: user.name,
+        roles: user.roles.map((r) => r.name),
+      },
+    };
+  }
+}
