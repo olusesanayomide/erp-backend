b078b39a23e5f6be723a06f39f342b16c33718ec
Ayomide Olusesan <kidd37214@gmail.com>
2026-01-19 09:02:34 +0100

    feat(auth): implement Role-Based Access Control (RBAC)
    - Created RolesGuard to enforce permission-based routing
    - Added @Roles decorator to tag endpoints with required permissions
    - Integrated RolesGuard with Reflector to support secure-by-default logic

---
+++ src/auth/guard/role.guard.ts
@@ -0,0 +1,31 @@
+import { ExecutionContext, CanActivate, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../decorator/role.decorator';
+import { Request } from 'express';
+import { UserPayload } from '../decorator/get-user.decorator';
+
+interface RequestWithUser extends Request {
+  user: UserPayload;
+}
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<string[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+    if (!requiredRoles) {
+      return true;
+    }
+    const request = context.switchToHttp().getRequest<RequestWithUser>();
+    const user = request.user;
+
+    if (!user || !user.roles) {
+      return false;
+    }
+    return requiredRoles.some((role) => user.roles.includes(role));
+  }
+}
